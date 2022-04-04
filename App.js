16eaa24e3dfc5f3b7004a8954caabd471e95cc7c
Yasmin <yazzmin0011@gmail.com>
2022-04-05 01:25:07 +0200

    .

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: "red", fontSize: 30 }}>hi</Text>
+      <Text>hi</Text>
+      <Text>hi</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginTop: 150,
   },
 });
